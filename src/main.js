d9d2fb711cb752327af6011ca44a822f81f7ade3
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-02-14 21:28:47 +0530

    [REFACTOR] : JS Role based views

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+import { Admin } from "./models/Admin";
+import { Member } from "./models/Member";
+
+const userSwitcher = document.getElementById("userSwitcher");
+const bookSection = document.getElementById("bookSection");
+const borrowedSection = document.getElementById("borrowedSection");
+
+let currentUser = new Member('John Doe', 'member@example.com');
+userSwitcher.addEventListener('change', (e) => {
+  const selected = e.target.value;
+  currentUser = selected === 'admin' ? new Admin('Admin', 'admin@example.com') : new Member('John Doe', 'member@example.com');
+  bookSection.style.display = (selected === "admin" ? "block" : "none");
+  borrowedSection.style.display = (selected === "member" ? "block" : "none");
+});
+
+// Initial Rendering
+bookSection.style.display = "none";
