64781ffcbf0b9fe7e1a240970e3579c52e0a858f
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-02-14 20:52:33 +0530

    [TASK] : Defined all required JS models and classes

---
+++ src/models/Member.js
@@ -0,0 +1,19 @@
+import { User } from "./User";
+
+export class Member extends User {
+  constructor(name, email) {
+    super(name, email);
+  }
+  
+  borrowBook(book) {
+    
+  }
+  
+  returnBook(bookId) {
+    
+  }
+  
+  getRole() {
+    return 'Member';
+  }
+}
